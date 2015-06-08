91edc76cf18aa456e4f44c579e840c0297b813ec
Tsvetan Koev <tsvetan.koev@gmail.com>
2015-06-08 17:54:27 +0300

    Forgot to add something
    Should save before commiting

--- src/player.py
+++ src/player.py
@@ -4,3 +4,10 @@ class Player(Creature):
 
     def __init__(self, name):
         super(name)
+        self._inventory = {}
+        self._equiped = {}
+        self._experience = 0
+        self._level = 1
+        self._strength = 1
+        self._inteligence = 1
+
