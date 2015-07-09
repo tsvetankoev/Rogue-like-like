da4201bfef055ed80a37c3c3c6e587e35a9e587a
Tsvetan Koev <tsvetan.koev@gmail.com>
2015-07-09 03:50:47 +0300

    Revert "fix inheritance"
    This reverts commit d671ea31fd02cdabe8078cce9b64cfc89f02cd02.

--- src/creature.py
+++ src/creature.py
@@ -1,12 +1,8 @@
 class Creature:
 
-    def __init__(self, name, hp, mana, strength, dexterity, inteligence):
+    def __init__(self, name):
         self._name = name
-        self._hp = hp
-        self._mana = mana
-        self._core_strength = strength
-        self._core_dexterity = dexterity
-        self._core_inteligence = inteligence
+        self._inventory = []
 
     def print_invetory(self):
         for item in self._inventory:
