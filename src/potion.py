b4327f2ad3a0720879e0d8165efea999a25fc30b
Tsvetan Koev <tsvetan.koev@gmail.com>
2015-07-11 22:56:02 +0300

    bugfix

--- src/potion.py
+++ src/potion.py
@@ -5,6 +5,6 @@ class Potion:
         self._value = value
 
     def use(self):
-        stat = self._name.replace("Potion of ", "")
+        stat = self.name.replace("Potion of ", "")
         stat = stat.lower()
         return (stat, self._value)
